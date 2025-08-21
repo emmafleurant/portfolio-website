64b93a96c773e109e494468b4db06976fcb572a9
Emmanuela <emmanuelafleurant0@gmail.com>
2025-08-21 19:48:56 -0400

    commit portfolio-website

---
+++ script.js
@@ -0,0 +1,21 @@
+function openPanel(evt, panelName) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tab-panel");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tab-button");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(panelName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+// Set the first tab as active on page load
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector(".tab-button").click();
+});
